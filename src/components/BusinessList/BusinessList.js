5070a31e2b25ccc5bfde6f72ea0783ef52c9c2cf
devrok <roland.kurz@gmx.at>
2018-04-28 16:44:28 +0200

    Part II using props

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,14 +6,11 @@ class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {
+          this.props.businesses.map(businessValue => {
+            return <Business business={businessValue}/>;
+          })
+        }
       </div>
     );
   }
